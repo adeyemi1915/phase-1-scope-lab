6d318a8198e6a97a64d66ca1f7aa609b0c57395b
adeyemi1915 <135009534+adeyemi1915@users.noreply.github.com>
2023-06-21 00:09:44 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+// customerName
+var customerName = "John"; // Declare the variable in global scope
+
+// upperCaseCustomerName()
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase(); // Access and modify the global customerName variable
+}
+
+// setBestCustomer()
+function setBestCustomer() {
+  bestCustomer = "not bob"; // Declare a global variable bestCustomer and assign it a value
+}
+
+// overwriteBestCustomer()
+function overwriteBestCustomer() {
+  bestCustomer = "new value"; // Modify the value of the global variable bestCustomer
+}
+
+// leastFavoriteCustomer and changeLeastFavoriteCustomer()
+const leastFavoriteCustomer = "Alice"; // Declare a constant in global scope
+
+function changeLeastFavoriteCustomer() {
+  // Trying to change the value of a constant will result in an error
+  leastFavoriteCustomer = "Bob";
+}
